6c3355dcb3ae74c954f82a0eb1eac701e1e77637
Andy <andychen2325@gmail.com>
2020-05-30 17:00:12 -0400

    update getURLtest.py

--- backend/utils/getURLtest.py
+++ backend/utils/getURLtest.py
@@ -5,11 +5,11 @@ from getURL import getURL
 class TestStringMethods(unittest.TestCase):
 
     def test_getURL(self):
-        self.assertEqual(getURL("https://google.com"), 'https://google.com')
+        self.assertEqual(getURL("https://globalnews.ca/news/7007185/coronavirus-exposure-warning-covid19-halifax/"), 'https://www.canadahelps.org/en/donate-to-coronavirus-outbreak-response/')
 
     def sample_getURL(self):
-        PLACEHOLDER_INPUT_URL = 'https://google.com'
-        PLACEHOLDER_OUTPUT_URL = 'https://google.com'
+        PLACEHOLDER_INPUT_URL = 'https://globalnews.ca/news/7007185/coronavirus-exposure-warning-covid19-halifax/'
+        PLACEHOLDER_OUTPUT_URL = 'https://www.canadahelps.org/en/donate-to-coronavirus-outbreak-response/'
         self.assertEqual(getURL(PLACEHOLDER_INPUT_URL),
                          PLACEHOLDER_OUTPUT_URL)
 
