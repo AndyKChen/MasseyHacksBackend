c1c34e0d3a97f8c60e60284a133dc2ccb481fd96
OrenLeung <oren.leung@gmail.com>
2020-05-30 16:59:29 -0400

    deleted the useless single url functions

--- backend/utils/getURLtest.py
+++ backend/utils/getURLtest.py
@@ -3,16 +3,6 @@ from getURL import getURL
 
 
 class TestStringMethods(unittest.TestCase):
-
-    def test_getURL(self):
-        self.assertEqual(getURL("https://google.com"), 'https://google.com')
-
-    def sample_getURL(self):
-        PLACEHOLDER_INPUT_URL = 'https://google.com'
-        PLACEHOLDER_OUTPUT_URL = 'https://google.com'
-        self.assertEqual(getURL(PLACEHOLDER_INPUT_URL),
-                         PLACEHOLDER_OUTPUT_URL)
-
     def loop_testURL(self):
         INPUT_URLS = ['https://google.com']
         OUTPUT_URLS = ["google.com"]
