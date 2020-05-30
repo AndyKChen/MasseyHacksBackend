f0f10594384b80c689f6b05d13c32d47ff46f4e4
OrenLeung <oren.leung@gmail.com>
2020-05-30 19:07:23 -0400

    sample model

--- backend/utils/googlescrape2.py
+++ backend/utils/googlescrape2.py
@@ -4,7 +4,7 @@ from googlesearch import search
 query = "covid-19"
 
 links = []
-for j in search(query, tld="co.in", lang = 'en', num=100, stop=100, pause=2):
+for j in search(query, tld="co.in", lang='en', num=100, stop=100, pause=2):
     links.append(j)
 
 print(links)
